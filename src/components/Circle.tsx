e25cfc62f3a52e4e2c54bc040f98ae60084dd92d
Sergei Zhilinskij <roadtomars2030@gmail.com>
2022-08-10 14:16:55 +0300

    Circle ready

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
 
 interface CircleProps {
   title: string;
@@ -6,28 +6,77 @@ interface CircleProps {
   list?: string[];
 }
 
+const sampleTitle = 'Почему мы ничего не понимаем?';
+const sampleList4 = [
+  'Эффект обратного результата',
+  'Ошибка базового процента',
+  'Конфа - буляция',
+  'Ошибка игрока',
+  'Ошибка выжившего',
+  'Ошибка меткого стрелка',
+  'Ошибка конъюнкции',
+  'Феномен Баадера – Майнхоф',
+];
+
+const sampleList1 = [
+  'Эффект слепого пятна',
+  'Эффект ложного согласия',
+  'Эффект третьего лица',
+  'Восприятие собственного выбора',
+  'Иллюзия контроля',
+];
+
 const Circle: FC<CircleProps> = ({
-  title,
+  title = sampleTitle,
   color = 'bg-slate-700',
-  list = [],
+  list = sampleList1,
 }) => {
+  const createList = (list: string[]): JSX.Element => {
+    const dividerClass =
+      'h-48 w-2 absolute bg-white top-0 left-1/2 origin-bottom';
+    const liClass = `text-white text-sm text-center h-48 w-20 absolute left-1/2 origin-bottom`;
+    const rotationParams: number[] = [];
+    const totalRotations = list.length * 2;
+    for (let i = 0; i < totalRotations; i++) {
+      rotationParams.push(Math.round((i * 360) / totalRotations));
+    }
+
+    return (
+      <ul>
+        {list.map((item, i) => (
+          <Fragment key={i}>
+            <li
+              className={dividerClass}
+              style={{
+                transform: `translateX(-50%) rotate(${
+                  rotationParams[i * 2]
+                }deg)`,
+              }}
+            />
+            <li
+              className={liClass}
+              style={{
+                transform: `translateX(-50%) rotate(${
+                  rotationParams[i * 2 + 1]
+                }deg) translateY(1rem)${
+                  rotationParams[i * 2 + 1] > 90 &&
+                  rotationParams[i * 2 + 1] < 270
+                    ? '  scaleX(-1) scaleY(-1) translateY(20rem)'
+                    : ''
+                }`,
+              }}
+            >
+              {item}
+            </li>
+          </Fragment>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <div className={`rounded-full h-96 w-96 relative ${color}`}>
-      <ul>
-        <li
-          className={`text-white h-48 absolute top-0 left-1/2 -translate-x-1/2 origin-bottom`}
-        >
-          Sample Text
-        </li>
-        <li
-          className={`text-white h-48 absolute top-0 left-1/2 -translate-x-1/2 origin-bottom rotate-45`}
-        >
-          Sample 2
-        </li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+      {createList(list)}
       <div
         className={`rounded-full h-44 w-44 bg-white 
 				absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
